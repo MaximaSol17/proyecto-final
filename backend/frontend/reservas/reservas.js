410333557911c923c553a8e681888536d68dcc35
Maxima Sol <mdecrescenzo@fi.uba.ar>
2025-07-17 15:11:07 -0300

    agrego nuevo html para el front de reservas

---
+++ backend/frontend/reservas/reservas.js
@@ -0,0 +1,10 @@
+[
+    {
+        "id": 1,
+        "cliente_id": 2,
+        "fecha_reserva": "20-08-2025",
+        "hora": "21:00",
+        "cantidad_personas": 4,
+        "estado": "pendiente"
+    }
+]
