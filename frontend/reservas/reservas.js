e808dd00af591da67798d4ecedfec74b28762ff0
Maxima Sol <mdecrescenzo@fi.uba.ar>
2025-07-19 16:15:39 -0300

    agrego archivo index.js para el templete de reservas

--- frontend/reservas/reservas.js
+++ frontend/reservas/reservas.js
@@ -1,10 +1,20 @@
-[
-    {
-        "id": 1,
-        "cliente_id": 2,
-        "fecha_reserva": "20-08-2025",
-        "hora": "21:00",
-        "cantidad_personas": 4,
-        "estado": "pendiente"
+const form = document.getElementById('form-reserva');
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();   //lo hago para evitar que la pagina se recargue al hacer submit
+    const data = Object.fromEntries(new FormData(form));  //esto va a tener los valores del formulario
+    
+    const res = await fetch('http://localhost:3000/reservas', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data)
+    });
+
+    //por las dudas, verifico que se haya creado bien
+    if (res.ok) {
+        alert('Reserva creada correctamente');
+        form.reset();
+    } else {
+        alert('Error al crear la reserva')
     }
-]
+});
