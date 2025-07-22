37a9c2c28f38657087fc003a9beb2658b00e2025
Maxima Sol <mdecrescenzo@fi.uba.ar>
2025-07-22 16:55:26 -0300

    agrego campo de Contraseña en tabla Clientes

--- backend/scripts/db.sql
+++ backend/scripts/db.sql
@@ -13,7 +13,8 @@ Create table clientes (
     apellido varchar(100) not null,
     email varchar(100) not null,
     edad int not null,
-    telefono int not null
+    telefono int not null,
+    contraseña varchar(10) not null
 );
 
 Create table menu (
@@ -26,10 +27,14 @@ Create table menu (
     pedidos int REFERENCES reservas (id)
 );
 
-insert into clientes (nombre, apellido, email, edad, telefono) values 
-('Adriana', 'Lopez', 'adrilopez@gmail.com', 37, 1123457654), 
-('Matias', 'Cerviño', 'matiascer@gmail.com', 23, 1156789008);
+insert into clientes (nombre, apellido, email, edad, telefono, contraseña) values 
+('Adriana', 'Lopez', 'adrilopez@gmail.com', 37, 1123457654, 'mamaypapa'), 
+('Matias', 'Cerviño', 'matiascer@gmail.com', 23, 1156789008, 'holasoyyo');
 
 insert into reservas (cliente_id, fecha_reserva, hora, cantidad_personas, estado) values 
-(1, 12-08-2025, 20-30, 4, 'confirmado'), 
-(2, 25-07-2025, 21-00, 2, 'pendiente');
+(2, '12-08-2025', '20:30', 4, 'confirmado'), 
+(3, '25-07-2025', '21:00', 2, 'pendiente');
+
+insert into menu (nombre, descripcion, precio, tipo, disponible, pedidos) values 
+('Fernet', 'bebida alcohólica elaborada con 70% coca-cola y 30% fernet', 5000, 'bebida alcohólica', 'si', 1);
+/*NO LO AGREGUÉ*/
