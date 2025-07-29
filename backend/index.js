a1a190812e6b818568f0e362f3eb21de5cf8851f
JBaxendale <jbaxendale@fi.uba.ar>
2025-07-29 20:08:52 +0000

    aniado metodos de CRUD

--- backend/index.js
+++ backend/index.js
@@ -1,37 +1,81 @@
 const express = require('express');
 const fs = require('fs');
-const path = require('path');
 const cors = require('cors');
-const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 const PORT = 3000;
-
 const archivoPedidos = path.join(__dirname, 'pedidos.json');
 
-
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
-
-app.get('/api/pedidos', (req, res) => {
-    const pedidos = JSON.parse(fs.readFileSync(archivoPedidos, 'utf-8'));
-    res.json(pedidos);
-});
-
-
+//Create
 app.post('/api/pedidos', (req, res) => {
     const nuevoPedido = req.body;
 
     if (!nuevoPedido.nombre_producto || !nuevoPedido.precio) {
-        return res.status(400).json({error: 'Faltan campos requeridos' });
+        return res.status(400).json({ error: 'Faltan campos requeridos' });
     }
 
-    const pedidos = JSON.parse(fs.readFileSync(archivoPedidos, 'utf-8'));
+    const pedidos = fs.existsSync(archivoPedidos)
+        ? JSON.parse(fs.readFileSync(archivoPedidos, 'utf-8'))
+        : [];
+
     nuevoPedido.id = Date.now();
     pedidos.push(nuevoPedido);
 
     fs.writeFileSync(archivoPedidos, JSON.stringify(pedidos, null, 2));
-
     res.status(201).json(nuevoPedido);
+});
+
+
+//Read
+app.get('/api/pedidos/:id', (req, res) => {
+    const pedidos = JSON.parse(fs.readFileSync(archivoPedidos, 'utf-8'));
+    const pedido = pedidos.find(p => p.id === parseInt(req.params.id));
+
+    if (!pedido) {
+        return res.status(404).json({ error: 'Pedido no encontrado' });
+    }
+
+    res.json(pedido);
+});
+
+//Update
+app.put('/api/pedidos/:id', (req, res) => {
+    const pedidos = JSON.parse(fs.readFileSync(archivoPedidos, 'utf-8'));
+    const index = pedidos.findIndex(p => p.id === parseInt(req.params.id));
+
+    if (index === -1) {
+        return res.status(404).json({ error: 'Pedido no encontrado' });
+    }
+
+    pedidos[index] = {
+        ...pedidos[index],
+        ...req.body,
+        id: pedidos[index].id
+    };
+
+    fs.writeFileSync(archivoPedidos, JSON.stringify(pedidos, null, 2));
+    res.json(pedidos[index]);
+});
+
+//Delete
+app.delete('api/pedidos/:id', (req, res) => {
+    const pedidos = JSON.parse(fs.readFileSync(archivoPedidos, 'utf-8'));
+    const id = parseInt(req.params.id);
+
+    const nuevosPedidos = pedido.filter(p => p.id !== id);
+
+    if (nuevosPedidos.length === pedidos.length) {
+        return res.status(404).json({ error: 'Pedido no encontrado' });
+    }
+
+    fs.writeFileSync(archivoPedidos, JSON.stringify(nuevosPedidos, null, 2));
+    res.json({ mensaje: 'Pedido eliminado con éxito' })
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
