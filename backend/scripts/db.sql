648c1a0f18feb356f576aaeac2e472e50762db0c
JBaxendale <jbaxendale@fi.uba.ar>
2025-07-31 18:10:36 +0000

    Merge finalizado y conflictos resueltos

--- backend/scripts/db.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE pedidos
-(
-    id SERIAL PRIMARY KEY,
-    reserva_id INT REFERENCES reservas(id),
-    nombre_producto VARCHAR(100) NOT NULL,
-    descripcion TEXT,
-    precio DECIMAL(10, 2) NOT NULL,
-    cantidad INT NOT NULL
-);
